8227e87162fea51f929769b081a703f75683eb5c
CodingHub242 <codinghub242@gmail.com>
2026-09-04 12:20:06 +0000

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(trustedProxies: ['*'], headers: \Illuminate\Http\Middleware\TrustProxies::HEADERS);
-
         $middleware->append(ForceHttps::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
